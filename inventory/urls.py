14450406a4d2b4bf9708759248eda889cde9dbac
Prabal <prabaldeshar@gmail.com>
2021-03-28 18:15:34 +0545

    Product detail page added

--- inventory/urls.py
+++ inventory/urls.py
@@ -4,5 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('product_form/', views.add_product, name='add_product'),
-    path('products/', views.get_products, name='get_products')
+    path('products/', views.get_all_products, name='get_products'),
+    path('categories/', views.get_all_catgories, name='get_categories'),
+    path('categories/<int:category_id>/', views.get_category_products, name='get_category_products'),
+    path('products/<int:product_id>/', views.product_detail, name='product_detail'),
 ]
--- inventory/views.py
+++ inventory/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from .forms import ProductForm
-from .models import Product
+from .models import Product,ProductCategory
 
 # Create your views here.
 def index(request):
@@ -26,7 +26,29 @@ def add_product(request):
     return render(request, 'inventory/product_form.html', {'form': form})
 
 
-def get_products(request):
+def get_all_products(request):
     products = Product.objects.all()
     print(1)
     return render(request, 'inventory/all_products.html', {'products': products})
+
+def get_all_catgories(request):
+    categories = ProductCategory.objects.all()
+    return render(request, 'inventory/all_categories.html', {'categories': categories})
+
+def get_category_products(request, category_id):
+    # products = Product.objects.filter(category=category)
+    category = ProductCategory.objects.get(id=category_id)
+    products = Product.objects.filter(category=category)
+    print(products)
+
+    context = {
+        'category' : category,
+        'products' : products
+    }
+
+    return render(request, 'inventory/category_products.html', context)
+
+def product_detail(request, product_id):
+    product = Product.objects.get(id=product_id)
+
+    return render(request, 'inventory/product_detail.html', {'product':product})
